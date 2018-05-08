7eed355c2929523b079749e870b938f022340b4f
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-08 10:46:03 +0700

    creation and hoisting

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var a = 'Hello World';
+b(); // Called b!
+console.log(a); // undefined -> hoisting 
+
+var a = 'Hello World'; // delete this line -> Uncaught ReferenceError: a is not defined
 
 function b() {
-    
+    console.log('Called b!');
 }
+
+// b();
+// console.log(a);
