9be513207bb7b4bcc5f378fdff2c1298f1baadfe
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-09 10:40:54 +0700

    operators

---
+++ 21_operators/app.js
@@ -0,0 +1,19 @@
+var a = 3 + 4;
+console.log(a);
+
+var b = 4 - 3;
+console.log(b);
+
+var c = 4 > 3;
+console.log(c);
+
+// How did Java engine do that?
+// '+' or other operation is a function
+
+// function +(a, b) {
+//     return // add the two #s
+// }
+
+// +(3,4) -> + 3 4 prefix notation
+// 3 + 4 infix
+// 3 4+ postfix
---
+++ 23_operator_precedence_associativity/app.js
@@ -0,0 +1,18 @@
+// var a = 3 + 4 * 5;
+// console.log(a)
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+// ------
+// var a = 2, b = 3, c = 4;
+
+// a = b = c; // assigment: right to left (Associativity)
+// // value memory of b equal value memory of c
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// ------
+var a = (3 + 4) * 5;
+console.log(a);
