5983fc8e25c77da4a811681e4a8b4ad662715dd3
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-11 11:03:17 +0700

    dangerous automatic semicolon inserting

---
+++ 41_automatic_semicolon_inserting/app.js
@@ -0,0 +1,15 @@
+function getPerson() {
+    
+    /* return // undefined
+    {
+        firstname: 'Tony'
+    } */
+
+    return {
+        firstname: 'Tony'
+    }
+}
+
+console.log(getPerson()); // undefined
+
+// return<Enter> -> Javascript engine automatically create semicolon ';'
