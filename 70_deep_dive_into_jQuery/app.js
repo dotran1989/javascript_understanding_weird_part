ebf7f0d4339a9d7c394fbf37ecf5ef5d8c504f8d
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-14 17:14:59 +0700

    dive deep into jQuery

---
+++ 70_deep_dive_into_jQuery/app.js
@@ -0,0 +1,14 @@
+var q = $("ul.people li");
+console.log(q);
+
+/* 
+jQuery.fn.init(3) [li, li, li, prevObject: jQuery.fn.init(1)]
+0:li
+1:li
+2:li
+length:3
+prevObject:jQuery.fn.init [document]
+__proto__
+:
+Object(0)
+ */
