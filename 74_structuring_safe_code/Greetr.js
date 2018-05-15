4ba28cd1a50c2b3f016823422fa4229b27ffbc33
Cuong Duong Ngoc Quoc <cuong.duong@titancorp.vn>
2018-05-15 15:42:24 +0700

    properties and chainable methods

--- 74_structuring_safe_code/Greetr.js
+++ 74_structuring_safe_code/Greetr.js
@@ -5,7 +5,68 @@
         return new Greetr.init(firstName, lastName, language); // generate new object with three properties
     }
 
-    Greetr.prototype = {}; // here is where I'll put any methods that I want to use inside my object that're returned from Greetr.
+    var supportedLangs = ['en', 'es']; // hidden to outside world, access via the closure.
+
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    }
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    }
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Iniciar sesión'
+    }
+
+    Greetr.prototype = { // Save memory. Here is where I'll put any methods and properties that I want to use inside my object that're returned from Greetr.init.
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+        validate: function() { // 'this' points to the object that's calling this function.
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+        greet: function(formal) {
+            var msg;
+
+            // if undefined or null it will be coerced to 'false'
+            if (formal) {
+                msg = this.formalGreeting();
+            } else {
+                msg = this.greeting();
+            }
+            if (console) {
+                console.log(msg);
+            }
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName());
+            }
+            return this;
+        },
+        setLang: function(lang) {
+            this.language = lang;
+
+            this.validate();
+
+            return this;
+        }
+    };
 
     Greetr.init = function(firstName, lastName, language) {
 
@@ -13,9 +74,11 @@
         self.firstName = firstName || '';
         self.lastName = lastName || '';
         self.language = language || 'en';
-    }; // any object returned from this function constructor will point where for it's prototype.
+    }; // this object's lexical environment is this whole function -> can access 'this' variable.
+    // Thank closures to keep 'this' variable even function invoke immediately.
 
     Greetr.init.prototype = Greetr.prototype; // any objects created with this function 'Greetr.init' should point 'Greetr.prototype' for its prototype chain.
+    // To use any methods on this propertype property
 
     global.Greetr = global.G$ = Greetr; // on the global object, these two names will point 'Greets' value.
 
--- 74_structuring_safe_code/app.js
+++ 74_structuring_safe_code/app.js
@@ -1,5 +1,54 @@
 var g = G$('John', 'Doe');
-console.log(g);
+g.greet().setLang('es').log();
+
+/* 
+g.greet().log().setLang('fr');
+
+Hello John!
+Logged in: John Doe
+Uncaught Invalid language
+ */
+
+/*
+g.greet().setLang('es').log();
+
+Hello John!
+Iniciar sesiÃ³n: John Doe 
+
+*/
 
 var h = Greetr('Harry', 'Porter');
-console.log(h);
+// console.log(h);
+h.greet(true);
+
+// Greetings, Harry Porter
+
+console.log(g);
+/* 
+Greetr.init {firstName: "John", lastName: "Doe", language: "es"}
+firstName:"John"
+language:"es"
+lastName:"Doe"
+__proto__:
+formalGreeting:ƒ ()
+fullName:ƒ ()
+greet:ƒ (formal)
+greeting:ƒ ()
+log:ƒ ()
+setLang:ƒ (lang)
+validate:ƒ ()
+__proto__:
+constructor:ƒ Object()
+hasOwnProperty:ƒ hasOwnProperty()
+isPrototypeOf:ƒ isPrototypeOf()
+propertyIsEnumerable:ƒ propertyIsEnumerable()
+toLocaleString:ƒ toLocaleString()
+toString:ƒ toString()
+valueOf:ƒ valueOf()
+__defineGetter__:ƒ __defineGetter__()
+__defineSetter__:ƒ __defineSetter__()
+__lookupGetter__:ƒ __lookupGetter__()
+__lookupSetter__:ƒ __lookupSetter__()
+get __proto__:ƒ __proto__()
+set __proto__:ƒ __proto__() 
+*/
